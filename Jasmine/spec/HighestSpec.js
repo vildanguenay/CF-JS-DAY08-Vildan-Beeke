7a945e462e69e1e4106a8b152476aab17f5b9b00
Vildan Günay <vildan-guenay@hotmail.com>
2019-07-25 10:46:20 +0200

    highest

---
+++ Jasmine/spec/HighestSpec.js
@@ -0,0 +1,6 @@
+describe("highest", function(){
+	it("should Return 4", function(){
+	expect(highest(1,2,3,4))
+		.toEqual(4);
+	});
+});
---
+++ Jasmine/src/highest.js
@@ -0,0 +1,6 @@
+function highest (n1,n2,n3,n4) {
+	return (Math.max(n1,n2,n3,n4));
+
+}
+
+highest(1,2,3,4);
