8a7aa1608ce165ef14f6fc8ef990b2272724d24b
Vildan Günay <vildan-guenay@hotmail.com>
2019-07-25 11:53:12 +0200

    degrees

---
+++ Jasmine/spec/degreesSpec.js
@@ -0,0 +1,7 @@
+describe("radianCalculator", function() {
+	it("should Return 60 for the number 30", function(){
+		expect(radianCalculator(30))
+		.toEqual(60);
+	});
+	
+});
---
+++ Jasmine/src/degrees.js
@@ -0,0 +1,9 @@
+function radianCalculator(degree) {
+			
+		radian = degree * (360/180);
+		return radian;
+}
+
+		
+		
+		
